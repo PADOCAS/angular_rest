9ad8b7cf982ec0c5cadd351b4ae1d3331dcaf7d8
Luciano <lucianodeciopacheco@gmail.com>
2023-12-26 14:23:01 -0300

    Serviço de interceptar as requisições e enviar o token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {LoginService} from "./service/login.service";
+import {HeaderInterceptorService} from "./service/header-interceptor.service";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [
     //Vamos prover o uso do LoginService para os componentes do módulo:
-    LoginService
+    LoginService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HeaderInterceptorService,
+      multi: true
+    }
   ],
   exports: [
     //Exportar o FormsModule para os componentes filhos enxergarem, sem precisar importar individualmente.
