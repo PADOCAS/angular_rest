153b8d3b0481738d805f058ccf7ac3bca7094c98
Luciano <lucianodeciopacheco@gmail.com>
2023-12-28 16:08:09 -0300

    Deletando Usuário e colocando um footer na grid de consulta de usuários

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -23,9 +23,22 @@ export class UsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     //Ao iniciar o Componente vai buscar os usuários para carregar a lista:
+    this.carregarUsuariosCadastrados();
+  }
+
+  public carregarUsuariosCadastrados() {
     this.usuarioService.getListUsuarios()
       .subscribe(data => {
         this.usuarios = data
+      });
+  }
+
+  public deleteUsuario(id:number) {
+    this.usuarioService.deleteUsuario(id)
+      .subscribe(data => {
+        console.log("Retorno do método delete: " + data);
+        //Após deletar, recarrega a lista de usuário para atualizar a tela!
+        this.carregarUsuariosCadastrados();
       });
   }
 }
--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -1,8 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Usuario} from "../../model/usuario";
 import {Observable} from "rxjs";
-import {AppComponent} from "../app.component";
 import {Constants} from "../../util/constants";
 
 @Injectable({
@@ -10,9 +9,23 @@ import {Constants} from "../../util/constants";
 })
 export class UsuarioService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  public getListUsuarios():Observable<Usuario[]> {
+  /**
+   * Método responsável por listar todos os usuários cadastrados (../usuario/listall)
+   *
+   */
+  public getListUsuarios(): Observable<Usuario[]> {
     return this.http.get<Usuario[]>(Constants.baseUrl + "listall")
+  }
+
+  /**
+   * Método responsável para chamar o delete Usuario da API (../usuario/{id})
+   *
+   * @param id (id do usuário -> number)
+   */
+  public deleteUsuario(id: number): Observable<any> {
+    return this.http.delete(Constants.baseUrl + id, {responseType: "text"});
   }
 }
