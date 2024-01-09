5b87ffa41db0978e243e3288497e028c448a6552
Luciano <lucianodeciopacheco@gmail.com>
2024-01-09 13:56:51 -0300

    Deixando o Toast no HTML

--- src/app/components/util/toast/toast.component.ts
+++ src/app/components/util/toast/toast.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {ToastService} from "../../../service/toast.service";
 import {NgbToast} from "@ng-bootstrap/ng-bootstrap";
-import {NgIf, NgTemplateOutlet} from '@angular/common';
+import {NgForOf, NgIf, NgTemplateOutlet} from '@angular/common';
 
 /**
  * HTML é montado pelo template direto aqui na classe typeScript (template)
@@ -12,38 +12,46 @@ import {NgIf, NgTemplateOutlet} from '@angular/common';
   imports: [
     NgbToast,
     NgTemplateOutlet,
-    NgIf
+    NgIf,
+    NgForOf
   ],
   styleUrl: './toast.component.css',
-  template: `
-    @for (toast of this.toastService.toasts; track toast) {
-      <ngb-toast
-        [class]="toast.classname"
-        [header]="toast.titulo"
-        [autohide]="toast.delay !== null ? true : false"
-        [delay]="toast.delay !== null ? toast.delay : 0"
-        (hidden)="this.toastService.remove(toast)">
-        <div style="display: flex; flex-direction: row;">
-<!--          Caso quiser por uma imagem para o ERRO, mas nosso espaço está curto não vale a pena: -->
-<!--          <svg *ngIf="toast.tipo === 'erro'" xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24"-->
-<!--               viewBox="0 0 24 24" style="margin-right: 5px;">-->
-<!--            <path-->
-<!--              d="M10.872 6.831l1.695 3.904 3.654-1.561-1.79 3.426 3.333.954-3.417 1.338 2.231 4.196-4.773-2.582-2.869 2.287.413-3.004-3.792-.726 2.93-1.74-1.885-2.512 3.427.646.843-4.626zm-.786-6.831l-1.665 9.119-6.512-1.228 3.639 4.851-5.548 3.294 7.108 1.361-.834 6.076 5.742-4.577 9.438 5.104-4.288-8.064 6.834-2.677-6.661-1.907 3.25-6.22-6.98 2.982-3.523-8.114z"-->
-<!--            />-->
-<!--          </svg>-->
-          <div style="display: flex;
-                flex-direction: row;
-                max-width: 300px;">
-            <p>{{ toast.mensagem }}</p>
-          </div>
-        </div>
-      </ngb-toast>
-    }
-  `,
+  templateUrl: './toast.component.html',
+  //Fizemos direto no templateURL -> classe html!!
+  // template: `
+  //   @for (toast of this.toastService.toasts; track toast) {
+  //     <ngb-toast
+  //       [class]="toast.classname"
+  //       [header]="toast.titulo"
+  //       [autohide]="toast.delay !== null ? true : false"
+  //       [delay]="toast.delay !== null ? toast.delay : 0"
+  //       (hidden)="this.toastService.remove(toast)">
+  //       <div style="display: flex; flex-direction: row;">
+  //         <!--          Caso quiser por uma imagem para o ERRO, mas nosso espaço está curto não vale a pena: -->
+  //         <!--          <svg *ngIf="toast.tipo === 'erro'" xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24"-->
+  //         <!--               viewBox="0 0 24 24" style="margin-right: 5px;">-->
+  //         <!--            <path-->
+  //         <!--              d="M10.872 6.831l1.695 3.904 3.654-1.561-1.79 3.426 3.333.954-3.417 1.338 2.231 4.196-4.773-2.582-2.869 2.287.413-3.004-3.792-.726 2.93-1.74-1.885-2.512 3.427.646.843-4.626zm-.786-6.831l-1.665 9.119-6.512-1.228 3.639 4.851-5.548 3.294 7.108 1.361-.834 6.076 5.742-4.577 9.438 5.104-4.288-8.064 6.834-2.677-6.661-1.907 3.25-6.22-6.98 2.982-3.523-8.114z"-->
+  //         <!--            />-->
+  //         <!--          </svg>-->
+  //         <div style="display: flex;
+  //               flex-direction: row;
+  //               max-width: 300px;">
+  //           <p>{{ toast.mensagem }}</p>
+  //         </div>
+  //       </div>
+  //     </ngb-toast>
+  //   }
+  // `,
+  //Declaramos direto no HOST as características do componente (position fixed, z-index, margin, top e padding, ficando na posição exata que queremos!
   host: {class: 'toast-container position-fixed top-0 end-0 p-3', style: 'z-index: 1200'},
 })
 export class ToastComponent {
 
   constructor(public toastService: ToastService) {
+  }
+
+  public getToastService() {
+    return this.toastService;
   }
 }
