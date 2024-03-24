7eb9bd4a5341dc99f5182676e965e51fa490d76c
Luciano <lucianodeciopacheco@gmail.com>
2024-03-24 18:23:06 -0300

    Gráfico Angular - Salários de Usuários (Melhorias)

---
+++ src/model/usuarioGrafico.ts
@@ -0,0 +1,9 @@
+export class UsuarioGrafico {
+  nome: string | any;
+  salario: string | any;
+
+  constructor(nome: any, salario: any) {
+    this.nome = nome;
+    this.salario = salario;
+  }
+}
